9f22e1328b26b360367fcc92aad2fbff29d11319
KISOFTAPPS <kisoftapps@outlook.com>
2022-09-28 12:32:11 -0700

    commit

--- src/home/page/Home.jsx
+++ src/home/page/Home.jsx
@@ -8,15 +8,23 @@ import {
     Section4,
 } from "../../home";
 
+import Fade from "react-reveal/Fade";
+
 export const Home = () => {
     return (
         <>
             <Navbar />
             <main>
                 <Section1 />
-                <Section2 />
-                <Section3 />
-                <Section4 />
+                <Fade bottom>
+                    <Section2 />
+                </Fade>
+                <Fade bottom>
+                    <Section3 />
+                </Fade>
+                <Fade bottom>
+                    <Section4 />
+                </Fade>
             </main>
             <Footer />
         </>
--- src/home/page/sections/Section4.jsx
+++ src/home/page/sections/Section4.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import bg3 from "../../../assets/img/bg-3.jpg";
-import { Map } from "../../";
+import bg4 from "../../../assets/img/bg-4.jpg"
 
 export const Section4 = () => {
     return (
@@ -9,15 +9,15 @@ export const Section4 = () => {
             className="min-h-screen w-full flex flex-col md:flex-row justify-center"
         >
             <div
-                className="w-96 h-96 md:w-7/12 md:h-auto"
+                className="w-96 h-96 md:w-7/12 md:h-auto shadow-xl"
                 style={{
                     backgroundImage: `url(${bg3})`,
                     backgroundSize: "cover",
                 }}
             ></div>
-            <div className="md:w-5/12 flex flex-col justify-center items-center p-5">
-                <div className="flex flex-col justify-center items-center gap-2">
-                    <span>LET'S REACH MORE ACHIEVEMENTS</span>
+            <div className="md:w-5/12 flex flex-col justify-center items-center gap-5 p-5 bg-green-50 text-green-900 shadow-xl" >
+                <div className="flex flex-col justify-center items-center gap-5">
+                    <span className="text-3xl font-bold">LET'S REACH MORE ACHIEVEMENTS</span>
                     <hr className="bg-black w-full" />
                     <div>
                         <p className="font-bold">Alonso Villarreal</p>
@@ -38,8 +38,9 @@ export const Section4 = () => {
                         <p>+1 210 793 6001</p>
                     </div>
                 </div>
-
+                <img src={bg4} className="rounded-lg border border-green-900 " />
             </div>
+            
         </section>
     );
 };
