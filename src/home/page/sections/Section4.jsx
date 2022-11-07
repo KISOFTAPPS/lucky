375e9deca268e69a890169d002ff99fbb649e0ee
KISOFTAPPS <kisoftapps@outlook.com>
2022-11-06 22:41:34 -0700

    cambio en usuarios de contacto

--- src/home/page/sections/Section4.jsx
+++ src/home/page/sections/Section4.jsx
@@ -19,24 +19,7 @@ export const Section4 = () => {
                 <div className="flex flex-col justify-center items-center gap-5">
                     <span className="text-3xl font-bold text-center">LET'S REACH MORE ACHIEVEMENTS</span>
                     <hr className="bg-black w-full" />
-                    <div>
-                        <p className="font-bold">Alonso Villarreal</p>
-                        <p className="font-semibold">President</p>
-                        <p>
-                            19230 STONE OAK PKWY STE 102, SAN ANTONIO, TX, 78258
-                        </p>
-                        <p>alonso_vh@luckytrgr.com</p>
-                        <p>+1 210 748 1007</p>
-                    </div>
-                    <div>
-                        <p className="font-bold">Juan Antonio Perdomo</p>
-                        <p className="font-semibold">Chief Operating Officer</p>
-                        <p>
-                            19230 STONE OAK PKWY STE 102, SAN ANTONIO, TX, 78258
-                        </p>
-                        <p>japerdomo@luckytrgr.com</p>
-                        <p>+1 210 793 6001</p>
-                    </div>
+                    
                 </div>
                 <img src={bg4} className="rounded-lg border border-green-900 " />
             </div>
